98ebda6cb2eb208cb360ca3f6afa51364f48839d
Adriana Broadnax <alwaysadrianna@gmail.com>
2021-09-20 19:40:53 -0400

    java

--- Develop/script.js
+++ Develop/script.js
@@ -4,18 +4,23 @@
 console.log({Password, generatePassword})
 var generateBtn = document.querySelector("#generate");
 
+// The vars needed
+var abcLower
+
 // Write password to the #password input
 function Numbers (){
-  var Password = prompt(" How long do you want the password? It must be at least 8 characters and no more than 128 characters!" );
+  var length = prompt(" How many characters would you like in your password? Pick a number between 8 and 128" );
   var min = 8
   var max = 128;
-  // 8 <= x < 128
-  console.log(Math.floor(Math.random() * max)) 
+  // length <8 * length > 128
+  if(length <8 * length >128){
+    alert("It must be between 8 and 128 characters!")
+  }
 }
 
-// Comfirm yes/no include lowercase
+// Comfirm lowercase
 function Lowercase (){
-  var Password = prompt ("Would you like any lowercase letters?");
+  var lowercase = prompt ("Would you like any lowercase letters?");
   var options = ["Yes", "No"]
   if (option[0] === "Yes") {
     console.log(options[0]);
@@ -25,7 +30,6 @@ function Lowercase (){
     console.log (options[1]);
     alert(`No ${Password} ${generatePassword}`)
   }
-
 }
 // comfirm yes/no uppercase
 // comfirm yes/no numeric
