603c975dfe64c7664e94b5b7fd2a52dac018a99e
Adriana Broadnax <alwaysadrianna@gmail.com>
2021-09-21 22:11:51 -0400

    java

--- Notes.md
+++ Notes.md
@@ -19,3 +19,57 @@ if (what your checking) {
 function functionName(parameter) {
     action
 }
+
+        // console.log("characterOptions: ", characterOptions)
+
+
+        //1. for loop through character options -> push random characters to resul
+
+        //2. For loop through guaranteed characters 
+        //result[i] = characterMust[i];
+
+        // return result.join('')
+
+    // needed to make the password
+
+    // if(confirm("Any upper case letters?")){
+    //     Array.prototype.push.apply(characters, lettersUpA);
+    // }
+    // if(confirm("What about lower case letters?")){
+    //     Array.prototype.push.apply(charcters, lettersLowA);
+    // }
+    // if(confirm("How about numbers?")){
+    //     Array.prototype.push.apply(characters, numberA);
+    // }
+    // if(confirm("Any symbols?")){
+    //     Array.prototype.push.apply(characters, symbolA);
+    // }
+    // if(characters.length===0){
+    //     alert("Please pick at LEAST one character!");
+    // }
+    // loop
+    // else{
+    //     for(var i=0; i<length; i++){
+    //         var random = Math.floor(Math.random()*characters.length);
+    //         result += characters[random];
+    //     }
+    // }
+    // }
+    // result
+    // generateBtn.getElementById("password").innerHTML = result;
+
+
+    // // Assignment Code
+    // var generateBtn = document.querySelector("#generate");
+
+    // // Write password to the #password input
+    // function writePassword() {
+    //     var password = generatePassword();
+    //     var passwordText = document.querySelector("#password");
+
+    //     passwordText.value = password;
+
+    // }
+
+    // Add event listener to generate button
+    // generateBtn.addEventListener("click", writePassword)
--- script.js
+++ script.js
@@ -64,60 +64,17 @@
             characterOptions = characterOptions.concat(letterUpA);
             characterMust.push(randomNumber(letterUpA))
         }
-
-        console.log("characterOptions: ", characterOptions)
-
-
-        //1. for loop through character options -> push random characters to resul
-
-        //2. For loop through guaranteed characters 
-        //result[i] = characterMust[i];
-
-        // return result.join('')
-
-
-    }
-
-    // needed to make the password
-
-    // if(confirm("Any upper case letters?")){
-    //     Array.prototype.push.apply(characters, lettersUpA);
-    // }
-    // if(confirm("What about lower case letters?")){
-    //     Array.prototype.push.apply(charcters, lettersLowA);
-    // }
-    // if(confirm("How about numbers?")){
-    //     Array.prototype.push.apply(characters, numberA);
-    // }
-    // if(confirm("Any symbols?")){
-    //     Array.prototype.push.apply(characters, symbolA);
-    // }
-    // if(characters.length===0){
-    //     alert("Please pick at LEAST one character!");
-    // }
-    // loop
-    // else{
-    //     for(var i=0; i<length; i++){
-    //         var random = Math.floor(Math.random()*characters.length);
-    //         result += characters[random];
-    //     }
-    // }
-    // }
-    // result
-    generateBtn.getElementById("password").innerHTML = result;
-
-
-    // Assignment Code
-    var generateBtn = document.querySelector("#generate");
-
-    // Write password to the #password input
-    function writePassword() {
-        var password = generatePassword();
-        var passwordText = document.querySelector("#password");
-
-        passwordText.value = password;
-
-    }
-
-    // Add event listener to generate button
-    generateBtn.addEventListener("click", writePassword);
+        if (userOptions.useLowerCase){
+            characterOptions = characterOptions.concat(letterLowA);
+            characterMust.push(randomNumber(letterLowA))
+        }
+        if (userOptions.useSymbols){
+            characterOptions = characterOptions.concat(useSymbols);
+            characterMust.push(randomNumber(symbolA))
+        }
+        if(userOptions.useNumbers){
+            characterOptions = characterOptions.concat(useNumbers);
+            characterMust.push(randomNumber(numberA))
+        }
+        
+        const buttonElement = document.getElementById('btn')
